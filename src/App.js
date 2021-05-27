02206f71682ebb8c8a027cae308b0a94e489fd01
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-05-27 18:13:27 -0400

    commit tree 0ec386c0a8d8670dd7f17df74098fc716b58331e

--- src/App.js
+++ src/App.js
@@ -34,8 +34,10 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App); 
+
+// export default connect( state => ({ items: state.items }) )(App); 
